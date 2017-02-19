03effb10094d55f47bcdb959bac081c35415cae1
hwlee2k <bless2k@gmail.com>
2017-02-19 16:02:16 +0900

    First Example

--- exec00/src/main.rs
+++ exec00/src/main.rs
@@ -1,3 +1,59 @@
+extern crate rand;
+
+use std::io;
+use std::io::Write;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    //---- io write excample
+    {
+        let stdout = io::stdout();
+        let mut handle = stdout.lock();
+        handle.write(b"Hello, Rust\n").expect("Error");
+    }
+
+    //---- Using println! macro
+    println!("> Start");
+
+    //---- Generate random number
+    let sec_no = rand::thread_rng().gen_range(0,100);
+    println!("> Sec No = {}", sec_no);
+
+    //---- Main Loop
+    loop {
+        println!("> Input No ");
+        let mut user_in = String::new();
+        io::stdin().read_line(&mut user_in).expect("@@Error: read_line");
+
+        println!("> Your Input: {}", user_in.trim());
+        let guessed_no: u32 = match user_in.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println! ("> Wrong Format, Ignored");
+                continue;
+            },
+        };
+
+        //---- u32 compare
+        {
+            if guessed_no < sec_no {
+                println!("> Smaller");
+            } else if guessed_no.eq(&sec_no) {
+                println!("> Match!");
+            } else if guessed_no > sec_no {
+                println!("> Bigger");
+            }
+        }
+
+        //---- u32 compare
+        match guessed_no.cmp(&sec_no) {
+            Ordering::Less => println!("> Smaller"),
+            Ordering::Greater => println!("> Bigger"),
+            Ordering::Equal => {
+                println!("> Match!");
+                break;
+            }
+        }
+    }
 }
